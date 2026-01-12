21d0df564d00f1ec2550a5fa6258782cd2e49af0
bhanureddy0106 <yadavallibhanureddy@gmail.com>
2026-01-12 13:52:01 +0530

    Add unit tests for Calculator class

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, calc.multiply(2, 3));
+    }
+}
